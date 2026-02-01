c1503ec023eba1eff15c2e7c4dc77e1ae4d8bc49
Loki Astari <Loki.Astari@gmail.com>
2026-02-01 11:32:54 -0800

    Updates based on review:
        https://codereview.stackexchange.com/questions/301153/json-flex-bison-parser/301170#301170

--- JSON-2/json2.cpp
+++ JSON-2/json2.cpp
@@ -5,29 +5,38 @@
 #include <iostream>
 
 
-bool checkJson(std::string const& fileName, std::istream& file)
+bool checkJson(std::istream& file, std::function<void(bool)>&& errorMsg)
 {
     ThorsAnvil::Json::Lexer     lexer(file);
     yy::Parser                  parser(lexer);
 
     bool valid = (parser.parse() == 0);
-    std::cout << fileName << ":\t\t" << ((valid ? "Valid" : "In Valid")) << "\n";
+    errorMsg(valid);
     return valid;
+}
+
+void errorPrinter(bool valid, std::string const& fileName)
+{
+    if (!valid) {
+        std::cerr << "Error: " << fileName << ":\t\tNot valid JSON.\n";
+    }
 }
 
 int main(int argc, char* argv[])
 {
     bool result = true;
     if (argc == 1) {
-        result = checkJson("", std::cin);
+        result = checkJson(std::cin, [](bool valid){errorPrinter(valid, "std::cin");});
     }
     else {
         for (int loop = 1; loop < argc; ++loop) {
             std::ifstream   file(argv[loop]);
             if (!file) {
-                std::cerr << "Invalid File: " << argv[loop] << "\n";
+                std::cerr << "Error: " << argv[loop] <<":\t\tCould not open file.\n";
+                result = false;
+                continue;
             }
-            if (!checkJson(argv[loop], file)) {
+            if (!checkJson(file, [filename = argv[loop]](bool valid){errorPrinter(valid, filename);})) {
                 result = false;
             }
         }
